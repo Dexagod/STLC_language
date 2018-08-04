b3ab90318187c1f9b26f3fa3c2ae08e89aa2379b
raddecke <ruben.dedecker@ugent.be>
2018-08-04 05:23:35 +0200

    variants

--- interpreter.py
+++ interpreter.py
@@ -1,5 +1,3 @@
-
-
 class Value: pass
 
 class Term: pass
@@ -11,9 +9,7 @@ class Const(Term, Value):
         self.T = T
 
     def __str__(self):
-        # return "Const(" + str(self.value) + ", " + str(self.T) + ")"
         return "c." + str(self.value) + "::" + str(self.T)
-        #return str(self.value)
 
     def __eq__(self, other):
         return (type(other) == type(self)) and (other.value == self.value)
@@ -33,7 +29,6 @@ class Var(Term):
         self.T = T
 
     def __str__(self):
-        # return "Var(" + str(self.labsel) + ")"
         return "v." + self.label
 
     def __eq__(self, other):
@@ -49,7 +44,6 @@ class Abs(Term, Value):
         self.body = body
 
     def __str__(self):
-        # return "Abs(" + str(self.param) + ", " + str(self.given_type) + ", " + str(self.body) + ")"
         return "(\\" + str(self.param) + ":" + str(self.given_type) + "." + str(self.body) + ")"
 
     def __eq__(self, other):
@@ -75,7 +69,6 @@ class App(Term):
         self.value = False
     
     def __str__(self):
-        # return "App(" + str(self.abs) + ", " + str(self.arg) + ")"
         return "(" + str(self.abs) + " " + str(self.arg) + ")"
 
     def __eq__(self, other):
@@ -115,21 +108,40 @@ class Record(Term):
     
     def get_values(self):
         return self.vals
+
+
+class Proj(Term):
+    def __init__(self, record, label):
+        self.record = record
+        self.label = label
+    
+    def __str__(self):
+        return str(self.record) + ".=>." + str(self.label)
+
+    def __eq__(self, other):
+        return (type(other) == type(self)) and other.record == self.record and other.label == self.label
+    
+    def get_record(self):
+        return self.record
+
+    def get_label(self):
+        return self.label
+    
   
 class Tag(Term):
-    def __init__(self, label, term, as_type):
-        self.label = label
+    def __init__(self, subtype_label, term, as_type):
+        self.subtype_label = subtype_label
         self.term = term
         self.as_type = as_type
     
     def __str__(self):
-        return "<" + str(self.label) + "=" + str(self.term) + "> as " + str(self.as_type)
+        return "<" + str(self.subtype_label) + "=" + str(self.term) + "> as " + str(self.as_type)
 
     def __eq__(self, other):
-        return (type(other) == type(self)) and other.label == self.label and other.term == self.term and other.as_type == self.as_type
+        return (type(other) == type(self)) and other.subtype_label == self.subtype_label and other.term == self.term and other.as_type == self.as_type
     
     def get_label(self):
-        return self.label
+        return self.subtype_label
 
     def get_term(self):
         return self.term
@@ -137,28 +149,43 @@ class Tag(Term):
     def get_as_type(self):
         return self.as_type
 
+# mapping is a map from 
 class Case(Term):
-    def __init__(self, term, mapping):
-        self.term = term
+    def __init__(self, tag, mapping):
+        self.tag = tag
         self.mapping = mapping
     
     def __str__(self):
-        string = "case " + str(self.term) + " of [" 
+        string = "case " + str(self.tag) + " of [" 
         for key in self.mapping:
-            string += "( " + key + " => " + self.mapping[key] + " ), "
+            string += "( " + str(key) + " ), "
         return string[:-2] + "]" 
 
     def __eq__(self, other):
-        return type(other) == type(self) and other.term == self.term and other.mapping == self.mapping
+        return type(other) == type(self) and other.tag == self.tag and other.mapping == self.mapping
 
-    def get_term(self):
-        return self.term
+    def get_tag(self):
+        return self.tag
     
     def get_mapping(self):
         return self.mapping
 
+class Map(Term):
+    def __init__(self, subtype_label, record, record_label):
+        self.subtype_label = subtype_label
+        self.record = record
+        self.record_label = record_label
 
-class Type: pass
+    def __str__(self):
+        return "<" + str(self.subtype_label) + "=" + str(self.record) + "> => " + self.record_label
+
+    def __eq__(self, other):
+        return (type(other) == type(self)) and other.subtype_label == self.subtype_label and other.record == self.record and other.record_label == self.record_label
+    
+    def __hash__(self):
+        return str(str(self.subtype_label) + str(self.record_label)).__hash__()
+
+class Type: pass    
 
 class SType(Type):
     def __init__(self, label):
@@ -191,7 +218,7 @@ class RType(Type):
     def __str__(self):
         string = "{"
         for key in self.types:
-            string += str(key) + " : " + str(self.types[key]) + ", "
+            string += str(key) + " ~ " + str(self.types[key]) + ", "
         string = string[:-2] + "}"
         return string
     
@@ -200,6 +227,9 @@ class RType(Type):
     
     def get_type(self, key):
         return self.types[key]
+    
+    def get_types(self):
+        return self.types
 
 
 class VType(Type):
@@ -225,7 +255,7 @@ class VType(Type):
 
 
 def typecheck_exp(exp, context):
-    
+    print("checking", exp, context)
     exp_class = type(exp)
 
     if exp_class == Const:
@@ -235,7 +265,7 @@ def typecheck_exp(exp, context):
         if exp.get_label() in context:
             return context[exp.get_label()]
         else:
-            raise Exception("Var" + str(exp) + " is not in context")
+            raise Exception(str(exp) + " is not in context")
     
     elif exp_class == Abs:
         new_context = context.copy()
@@ -247,29 +277,62 @@ def typecheck_exp(exp, context):
         record_type = RType()
         value_dict = exp.get_values()
         for key in value_dict:
-            if type(key) != Const:
-                raise Exception("Record key is not a constant")
-            else:
-                record_type.set_type(key, typecheck_exp(value_dict[key], context))
+            record_type.set_type(key, typecheck_exp(value_dict[key], context))
         return record_type
     
+    elif exp_class == Proj:
+        record_type = typecheck_exp(exp.record, context)
+        if not exp.label in record_type.get_types():
+            raise Exception("label " + str(exp.label) + " was not found in RType " + str(record_type))
+        else:
+            return record_type.get_type(exp.label)
+    
     elif exp_class == Tag:
-        if not issubclass(exp.get_as_type(), Type):
+        if not issubclass(type(exp.get_as_type()), Type):
             raise Exception("Tag " + str(exp) + " did not receive a correct type")
-        elif not type(exp.get_label()) != Const:
-            raise Exception("Tag " + str(exp) + " did not receive a Const as label")
-        
-        
+        return exp.get_as_type()
+    
+    elif exp_class == Case:
+        # TODO:: WHAT IF TAG IS NOT A TAG BUT A VAR TO BE REPLACED OR STH
+        supertype = exp.tag.as_type
+        tag_subtype_label = exp.tag.subtype_label
+        subtype = supertype.get_type(tag_subtype_label)
 
+        if type(subtype) != RType:
+            raise Exception("The subtype of the variant was no record type")
+
+        shared_type = None
+        for m in exp.mapping:
+            subT = supertype.get_type(m.subtype_label)
+            found_type = subT.get_type(m.record_label)
+            if shared_type == None:
+                shared_type = found_type
+            else:
+                if shared_type != found_type:
+                    raise Exception("The case statement can return more than one type")
+
+        record_label = ""
+        for m in exp.mapping:
+            if m.subtype_label == tag_subtype_label:
+                record_label = m.record_label
+            
+
+        if record_label == "":
+            raise Exception("There was no mapping for this subtype")
+
+        return subtype.get_type(record_label)
+        
     elif exp_class == App:
         t_abs = typecheck_exp(exp.abs, context)
         t_arg = typecheck_exp(exp.arg, context)
+
+        # THE PROBLEM IS THAT THE ARGUMENT ONLY GIVES TYPE::int for example, however we do not posess the label itself, so are unable to return the
         if type(t_abs) != CType:
             string = str("Application " + str(exp) + " first type is not a CType but " + str(t_abs) + " and second type is " + str(t_arg))
             raise Exception(string)
         
         if t_arg != t_abs.left:
-            string = str("Application " + str(exp) + " types do not match")
+            string = str("Application " + str(exp) + " types do not match: left_type = " + str(t_abs) + " | right_type = " + str(t_arg))
             raise Exception(string)
         return t_abs.right
     
@@ -293,18 +356,41 @@ def eval_exp(exp):
         return exp
     
     elif exp_class == Record:
-        # print("E-PROJ + E-RCD")
+        # print("E-RCD")
         for key in exp.get_values():
             new_value = eval_exp(exp.get_value(key))
             exp.set_value(key, new_value)
         return exp
+
+    elif exp_class == Proj:
+        # print("E-PROJ")
+        exp.record = eval_exp(exp.record)
+        return exp.record.get_value(exp.label)
+
     
     elif exp_class == Tag:
-        # print("E-PROJ + E-RCD")
-        for key in exp.get_values():
-            new_value = eval_exp(exp.get_value(key))
-            exp.set_value(key, new_value)
+        # print("E-VARIANT")
+        exp.term = eval_exp(exp.term)
         return exp
+
+    elif exp_class == Case:
+        # print("E-CASE")
+        exp.tag = eval_exp(exp.tag)
+        if type(exp.tag) == Tag:
+            mapping = exp.mapping
+            tag = exp.tag
+            subtype_label = tag.subtype_label
+            record_label = ""
+            for m in mapping:
+                if m.subtype_label == subtype_label:
+                    record_label = m.record_label
+            if record_label == "":
+                raise Exception("subtype label not found in Variant")
+            
+            return eval_exp(Proj(tag.term, record_label))
+        else:
+            return exp
+        
 
     #  NAKIJKEN OF VOLDOET AAN DE REGELS
     elif exp_class == App:
@@ -334,7 +420,10 @@ def eval_exp(exp):
 
         if type(e_abs) == Record:
             print("E-PROJRCD")
-            return e_abs.get_value(e_arg)
+            try:
+                return e_abs.get_value(e_arg)
+            except Exception:
+                raise Exception("Record " + str(e_abs) + " does not contain label " + str(e_arg))
             
 
         if not exp.is_value and (issubclass(type(e_abs), Value) and issubclass(type(e_arg), Value)):
@@ -359,9 +448,23 @@ def application_substitution(abstraction_body, abstraction_param, arg):
             return abstraction_body
     
     if type(abstraction_body) is Record:
-        for key in abstraction_body:
-            abstraction_body[key] = application_substitution(abstraction_body[key], abstraction_param, arg)
+        for key in abstraction_body.get_values():
+            substituted_value = application_substitution(abstraction_body.get_value(key), abstraction_param, arg)
+            abstraction_body.set_value(key, substituted_value)
+        return abstraction_body
 
+    if type(abstraction_body) is Proj:
+        abstraction_body.record = application_substitution(abstraction_body.record, abstraction_param, arg)
+        return abstraction_body
+    
+    # TODO:: RULES FOR VARIANTS NEED TO BE ADDED
+    if type(abstraction_body) is Tag:
+        abstraction_body.term = application_substitution(abstraction_body.term, abstraction_param, arg)
+        return abstraction_body
+    
+    if type(abstraction_body) is Case:
+        abstraction_body.tag = application_substitution(abstraction_body.tag, abstraction_param, arg)
+        return abstraction_body
 
     elif type(abstraction_body) is Abs:
         # print("SUBST - ABS")
@@ -382,53 +485,81 @@ def application_substitution(abstraction_body, abstraction_param, arg):
         abstraction_body.arg = application_substitution(app_arg, abstraction_param, arg)
         return abstraction_body
 
+def evaluate_expression(exp):
+    
+    evaluated = eval_exp(exp)
+    while evaluated != exp:
+        exp = evaluated
+        evaluated = eval_exp(exp)
 
+    return evaluated
 
 
 if __name__ == "__main__":
     
 
-    int_type = SType("int")
-    int_int_type = CType(int_type, int_type)
-    int_int_to_int_int_type = CType(int_int_type, int_int_type)
+    # int_type = SType("int")
+    # int_int_type = CType(int_type, int_type)
+    # int_int_to_int_int_type = CType(int_int_type, int_int_type)
 
-    const_zero = Const(0, int_type)
-    const_succ = Const("succ", int_int_type)
+    # const_zero = Const(0, int_type)
+    # const_succ = Const("succ", int_int_type)
 
-    one = Abs(Var("s"), int_int_type, Abs( Var("z"), int_type, App(Var("s"), Var("z"))))
-    two = Abs(Var("s"), int_int_type, Abs( Var("z"), int_type, App(Var("s"), App(Var("s"), Var("z") ) ) ) )
-    two2 = Abs(Var("s"), int_int_type, Abs( Var("z"), int_type, App(Var("s"), App(Var("s"), Var("z") ) ) ) )
+    # one = Abs(Var("s"), int_int_type, Abs( Var("z"), int_type, App(Var("s"), Var("z"))))
+    # two = Abs(Var("s"), int_int_type, Abs( Var("z"), int_type, App(Var("s"), App(Var("s"), Var("z") ) ) ) )
+    # two2 = Abs(Var("s"), int_int_type, Abs( Var("z"), int_type, App(Var("s"), App(Var("s"), Var("z") ) ) ) )
 
-    addition = Abs(Var("m"), int_int_to_int_int_type, Abs(Var("n"), int_int_to_int_int_type, Abs(Var("s"), int_int_type, Abs( Var("z"), int_type, App( App(Var("m"), Var("s")), App( App( Var("n"), Var("s")), Var("z") )) ) ) ) )
+    # addition = Abs(Var("m"), int_int_to_int_int_type, Abs(Var("n"), int_int_to_int_int_type, Abs(Var("s"), int_int_type, Abs( Var("z"), int_type, App( App(Var("m"), Var("s")), App( App( Var("n"), Var("s")), Var("z") )) ) ) ) )
 
-    successor = Abs(Var("n"), int_int_to_int_int_type, Abs(Var("s"), int_int_type, Abs( Var("z"), int_type, App( Var("s"), App( App( Var("n"), Var("s")), Var("z") )) ) ) )
+    # successor = Abs(Var("n"), int_int_to_int_int_type, Abs(Var("s"), int_int_type, Abs( Var("z"), int_type, App( Var("s"), App( App( Var("n"), Var("s")), Var("z") )) ) ) )
 
-    oneplustwo = App(App(addition, one), two)
+    # oneplustwo = App(App(addition, one), two)
 
-    oneplustwowithargs = App(App(App(App(addition, one), two), Const("succ", int_int_type)), Const(0, int_type))
+    # oneplustwowithargs = App(App(App(App(addition, one), two), Const("succ", int_int_type)), Const(0, int_type))
 
-    twoplustwowithargs = App(App(App(App(addition, two), two2), Const("succ", int_int_type)), Const(0, int_type))
+    # twoplustwowithargs = App(App(App(App(addition, two), two2), Const("succ", int_int_type)), Const(0, int_type))
 
-    successorone = App(App(App(successor, one), Const("succ", int_int_type)), Const(0, int_type))
+    # successorone = App(App(App(successor, one), Const("succ", int_int_type)), Const(0, int_type))
 
-    # TODO:: FIX IN PARSER
-    record_dict = dict()
-    record_dict[Const("1", SType("String"))] = Abs(Var("x"), int_int_type, Var("x"))
-    record_dict[Const("2", SType("String"))] = App(Abs(Var("x"), int_int_type, Var("x")), Const(2, SType("Integer")))
-    record_dict[Const(3, SType("Integer"))] = Const("4", SType("String"))
-    record = Record(record_dict)
+    # # TODO:: FIX IN PARSER
+    # record_dict = dict()
+    # record_dict[1] = Abs(Var("x"), int_type, Var("x"))
+    # record_dict[2] = App(Abs(Var("x"), int_type, Var("x")), Const(2, SType("int")))
+    # record_dict["test"] = Const("4", SType("str"))
+    # record = Record(record_dict)
+    # record_application = Proj( Record(record_dict), "test")
 
-    print(eval_exp(record))
-    print(typecheck_exp(record, dict()))
-
-    # exp = twoplustwowithargs
-    # print("TYPECHECKING")
-    # print(typecheck_exp(exp, dict()))
-
-    # evaluated = eval_exp(exp)
-    # while (evaluated != exp):
-    #     print("NEXTWHILE")
-    #     exp = evaluated
-    #     evaluated = eval_exp(evaluated)
+    # record_type = typecheck_exp(record, dict(""))
+    # record_application2 = Proj( App(Abs( Var("x"), record_type, Var("x")), Record(record_dict)), 1)
     
-    # print(evaluated)
+    rd3 = dict()
+    rd3["a"] = Const("KOK", SType("string"))
+    rd3["b"] = Const(2, SType("int"))
+    r3 = Record(rd3)
+    r3type = typecheck_exp(r3, dict())
+
+
+    rd1 = dict()
+    rd1["a"] = Var("z") # Const("KOK", SType("string"))
+    rd1["b"] = Const(2, SType("int"))
+    r1 = Record(rd1)
+    # r1type = typecheck_exp(r1, dict())
+    rd2 = dict()
+    rd2["c"] = Const("kuk", SType("string"))
+    rd2["d"] = Const(4, SType("int"))
+    r2 = Record(rd2)
+    r2type = typecheck_exp(r2, dict())
+    td = dict()
+    td["one"]=r3type#r1type
+    td["two"]=r2type
+    vtype = VType()
+    vtype.types = td
+    print("vtype", vtype)
+    a = Tag("one", Var("x"), vtype)
+    mapping = set()
+    mapping.add(Map("one", r1, "a"))
+    mapping.add(Map("two", r2, "c"))
+    # getname = App(Abs( Var("x"), vtype, Case(Var("x"), mapping)), Tag("one", Record(rd1), vtype))
+    getname = App(Abs( Var("z"), SType("string"), App(Abs( Var("x"), vtype, Case(Var("x"), mapping)), Tag("one", Record(rd1), vtype))), Const("appel",SType("string")))
+    print(evaluate_expression(getname))
+    print(typecheck_exp(getname, dict()))
